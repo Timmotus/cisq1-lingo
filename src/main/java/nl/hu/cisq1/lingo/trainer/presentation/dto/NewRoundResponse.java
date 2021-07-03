ab066bc163f18f63ca720fce8038e6ecb12d9386
Timmotus <tim.vandijk@student.hu.nl>
2021-07-03 20:07:51 +0200

    prettyfied responses

--- src/main/java/nl/hu/cisq1/lingo/trainer/presentation/dto/NewRoundResponse.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/presentation/dto/NewRoundResponse.java
@@ -2,16 +2,15 @@ package nl.hu.cisq1.lingo.trainer.presentation.dto;
 
 import nl.hu.cisq1.lingo.trainer.application.dto.GameState;
 import nl.hu.cisq1.lingo.trainer.domain.GameStatus;
-import nl.hu.cisq1.lingo.trainer.domain.Hint;
 
 public class NewRoundResponse {
     public Long id;
     public GameStatus status;
-    public Hint hint;
+    public String hint;
 
     public NewRoundResponse(GameState gameState) {
         this.id = gameState.getId();
         this.status = gameState.getStatus();
-        this.hint = gameState.getHint();
+        this.hint = gameState.getHint().toString();
     }
 }
